d11a5bd90d4aff3494b9c17f7b4edf0138637719
EdyWDev <edyta.wysocka.1994@gmail.com>
2023-03-23 11:09:51 +0100

    added new functionalities to the activity

--- app/src/main/java/com/example/homebar/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/example/homebar/welcome/WelcomeViewModel.kt
@@ -1,21 +1,86 @@
 package com.example.homebar.welcome
 
 import android.os.Parcelable
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.homebar.model.PresenceOfAlcoholCategory
+import com.example.homebar.recipedetails.model.DetailsExtraData
+import com.example.homebar.recipedetails.model.ExtraDataConstDetails
+import com.example.homebar.recipesearch.model.Drinks
+import com.example.homebar.recipesearch.model.Recipe
+import com.example.homebar.recipesearch.service.RecipeRepository
+import com.example.homebar.room.DrinkDatabaseRepository
+import com.example.homebar.room.DrinkEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle
-): ViewModel() {
+    private val savedStateHandle: SavedStateHandle,
+    private val recipeRepository: RecipeRepository,
+    private val drinkDbRepository: DrinkDatabaseRepository
+) : ViewModel() {
+    private val extraData: DetailsExtraData? =
+        savedStateHandle[ExtraDataConstDetails.EXTRA_DATA_RECIPE_DETAILS]
 
+    private val _imageRandomLD = MutableLiveData<String>()
+    val imageRandom: LiveData<String> = _imageRandomLD
 
+    private val _drinkNameRandomLD = MutableLiveData<String>()
+    val drinkNameRandom: LiveData<String> = _drinkNameRandomLD
+
+    private val _idDrinkRandomLD = MutableLiveData<Int>()
+    val idDrinkRandom: LiveData<Int> = _idDrinkRandomLD
+    fun searchRandom() {
+        viewModelScope.launch {
+            try {
+                val responseRandom = recipeRepository.getRandomCocktail()
+                responseRandom?.idDrink.let { _idDrinkRandomLD.value = it }
+                responseRandom?.strDrinkThumb.let {
+                    _imageRandomLD.value = it
+
+                }
+                responseRandom?.strDrink.let {
+                    _drinkNameRandomLD.value = it
+                }
+//                drinkDbRepository.saveDrink(
+//                    DrinkEntity(
+//                        drinkId = responseRandom?.idDrink ?: 0,
+//                        drinkName = responseRandom?.strDrink ?: "Example text",
+//                        drinkDesc = responseRandom?.strInstructions ?: "Example instruction",
+//                        drinkUrl = responseRandom?.strDrinkThumb ?: "example URL"
+//                    ))
+            } catch (e: Exception) {
+                // Retrofit error
+                Log.e("MYAPP", "exception", e)
+            }
+        }
+
+    }
+    
+    
+    fun searchRandomById() {
+        viewModelScope.launch {
+
+            try {
+                val getIdDrinkRandom = recipeRepository.getRandomCocktail()
+                getIdDrinkRandom?.idDrink.let {
+                    _idDrinkRandomLD.value = it
+                }
+            } catch (e: Exception) {
+                // Retrofit error
+                Log.e("MYAPP", "exception", e)
+
+            }
+        }
+    }
 }
+
+
+
+
 
 
 
--- app/src/main/java/com/example/homebar/welcome/ui/WelcomeActivity.kt
+++ app/src/main/java/com/example/homebar/welcome/ui/WelcomeActivity.kt
@@ -1,12 +1,21 @@
 package com.example.homebar.welcome.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.homebar.R
 import com.example.homebar.databinding.WelcomeActivityBinding
-import com.example.homebar.model.PresenceOfAlcoholCategory.FREE_OF_ALCOHOL
 import com.example.homebar.model.PresenceOfAlcoholCategory.WITH_ALCOHOL
+import com.example.homebar.navigation.HomeBarNavigationManager.navigateToFavouriteDrink
+import com.example.homebar.navigation.HomeBarNavigationManager.navigateToRecipeDetails
 import com.example.homebar.navigation.HomeBarNavigationManager.navigateToRecipeSearch
+import com.example.homebar.recipedetails.model.DetailsExtraData
+import com.example.homebar.recipesearch.model.Drinks
 import com.example.homebar.recipesearch.model.RecipeSearchExtraData
 import com.example.homebar.welcome.WelcomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +26,7 @@ class WelcomeActivity : AppCompatActivity() {
 
     private val viewModel: WelcomeViewModel by viewModels()
     private lateinit var binding: WelcomeActivityBinding
+    private lateinit var drinksList: List<Drinks>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +42,55 @@ class WelcomeActivity : AppCompatActivity() {
             )
         }
 
-        /*binding.drinkFreeBT.setOnClickListener {
-            navigateToRecipeSearch(
-                extraData = RecipeSearchExtraData(
-                    FREE_OF_ALCOHOL
-                )
+        //JAK TO ROZROZNIC NA PODSTAWIE TYPU EXTRA DATY?
+
+        binding.favouriteBT.setOnClickListener {
+            navigateToFavouriteDrink(
+                extraData = DetailsExtraData
+                    (viewModel.idDrinkRandom.value ?: 0)
             )
-        }*/
+       //     val data = viewModel.getDrinkFromDatabase()    //DLACZEGO TO NIE DZIALA I CO TO ZA FCJA?
+            //Toast.makeText(this, data, Toast.LENGTH_LONG).show()
+        }
+
+        val imageRandomObserver = Observer<String> { value ->
+            Glide.with(binding.root.context)
+                .load(value)
+                .transition(DrawableTransitionOptions.withCrossFade(75))
+                .into(binding.drinkImageRandom)
+            setVisibilityRandomDrink()
+        }
+        viewModel.imageRandom.observe(this, imageRandomObserver)
+
+        val drinkNameRandomObserver = Observer<String> { value ->
+            binding.drinkNameRandom.text = value
+        }
+        viewModel.drinkNameRandom.observe(this, drinkNameRandomObserver)
+
+
+        binding.randomCocktailBT.setOnClickListener {
+            viewModel.searchRandom()
+        }
+
+        val idDrinkObserver = Observer<Int> {}
+        viewModel.idDrinkRandom.observe(this, idDrinkObserver)
+
+        binding.moreInformationBT.setOnClickListener {
+            navigateToRecipeDetails(DetailsExtraData(viewModel.idDrinkRandom.value ?: 0))
+        }
+
     }
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    private fun setVisibilityRandomDrink(){
+        binding.drinkImageRandom.visibility = View.VISIBLE
+        binding.drinkNameRandom.visibility = View.VISIBLE
+        binding.moreInformationBT.visibility = View.VISIBLE
+    }
+
+
 }
+
